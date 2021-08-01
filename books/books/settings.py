45f62205e887f37b387e37e9abb618596a9c5f3e
Katarzyna <katarzynapiskala1@gmail.com>
2021-08-01 11:39:37 +0200

    changes in storing keys, packages, and adding bootstrap

--- books/books/settings.py
+++ books/books/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
-API_KEY = os.environ['API_KEY']
+SECRET_KEY = config('SECRET_KEY')
+API_KEY = config('API_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'pages',
 
     'django_filters',
+    'decouple',
 ]
 
 MIDDLEWARE = [
