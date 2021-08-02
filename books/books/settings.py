a14f6728e641cfa9f5751c38da92371908c2835a
Katarzyna <katarzynapiskala1@gmail.com>
2021-08-03 00:55:02 +0200

    changes to deploy heroku

--- books/books/settings.py
+++ books/books/settings.py
@@ -28,7 +28,7 @@ API_KEY = config('API_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://managing-books-application.herokuapp.com/']
 
 
 # Application definition
